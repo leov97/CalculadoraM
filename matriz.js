0dcd594f3fcc5a001824120bd479b69cc5167b47
Leonel <ronnyportillo59@gmail.com>
2023-02-26 01:52:32 -0600

    avance suma de matrices

--- matriz.js
+++ matriz.js
@@ -1,21 +1,31 @@
-class calcuos{
-    dat(){
+class CreateMatrix{
+    constructor(id) {
+        this.id= id
+    }
+    datosInputs(){
+        // optengo los valores de mis inputs que definen las dimenciones de la matriz 
         const col= parseInt(document.querySelector('#col').value)
         const row= parseInt(document.querySelector('#row').value)
         this.values=[col, row]
+      
     }
-//---------------------------------------------
-    tab(){
+//*****************************************************************************
+    tablaMatrix(){
+        //creo la tabla pa la matriz de inputs 
         const table= document.createElement("table")// con esto creo la tabla 
+        table.setAttribute("id", `${this.id}`);  //le agrego un id a mi tabla
         const head= table.createTHead()//con esto creo su encabezado
         const r= head.insertRow()
         const c= r.insertCell()
+        //llamo los valores de mi metodo datosInputs
         const co=this.values[0]
         const ro=this.values[1]
+        /*se hace un ciclo for para que me cree la cantidad de inputs que 
+        el usuario especifique en las dimenciones*/
         for(let i=0; i<ro; i++){
             const r= table.insertRow()
             const c= r.insertCell()
-        
+//----------------------------------------------------------------------------        
             for(let j=0; j<co; j++){
               const c= r.insertCell()
               const inputs= document.createElement("input")//creo el input 
@@ -33,42 +43,63 @@ class calcuos{
           //tab.innerHTML=""
           tab.appendChild(table)
           //tab.appendChild(document.createElement("br"));
-
+       //   i++
     }
-//------------------------------------------------------------
+//*****************************************************************************
     matric(){
-       
 
     }
-//---------------------------------------------
-    alcul(){
-    }
+//*****************************************************************************
+   
 }
+//----------------------------------------------------------------------------
 
 function myMatrix() {
-
-
-tab.innerHTML="suma de matrices"
-const myclass= new calcuos()
-myclass.dat()
-myclass.tab()
-const mycla= new calcuos()
-mycla.dat()
-mycla.tab()
-
+    tab.innerHTML="suma de matrices"
+    const myclass= new CreateMatrix("id1")
+    myclass.datosInputs()
+    myclass.tablaMatrix()
+//----------------------------------------------------------------------------
+    const mycla= new CreateMatrix("id2")
+    mycla.datosInputs()
+    mycla.tablaMatrix()
+//----------------------------------------------------------------------------
     const calcu=document.createElement("input")
     calcu.type="submit"
     calcu.name="calcu"
     calcu.id="calculo"
-    tab.appendChild(calcu)
 
-const calculo = document.getElementById("calculo");
-calculo.addEventListener("click", function(){
+    
+//----------------------------------------------------------------------------
+    const CalcularSuma= document.getElementById("tab")
+    CalcularSuma.appendChild(calcu)
 
-    console.log("hola como estas ")
-}
-)
+    const calculo = document.getElementById("calculo");
+    calculo.addEventListener("click", function(){
+        //llamo la tabla con id= "id1"
+        const tablaId1 = document.getElementById("id1");
+        const matrizValores = [];
+        
+        for (const tr of tablaId1.querySelectorAll('tr')) {
+          const filaValores = [];
+          for (const td of tr.querySelectorAll('td')) {
+            const input = td.querySelector('input');
+            if (input) {
+              filaValores.push(input.value);
+            }
+          }
+          if (filaValores.length > 0) {
+            matrizValores.push(filaValores);
+          }
+        }
+        
+        console.log(matrizValores);
+}        /*const sumaMatrix=document.createElement("table")
+        const filas=sumaMatrix.querySelector("tr")
+        const valores=[]*/
 
+        
+    )
 }
 
 

4413117f8cb3833ee4fb79779c9b868c2279cfcd
Leonel <ronnyportillo59@gmail.com>
2023-03-02 18:21:54 -0600

    suma de matrices lista

--- matriz.js
+++ matriz.js
@@ -2,12 +2,12 @@ class CreateMatrix{
     constructor(id) {
         this.id= id
     }
+    
     datosInputs(){
         // optengo los valores de mis inputs que definen las dimenciones de la matriz 
         const col= parseInt(document.querySelector('#col').value)
         const row= parseInt(document.querySelector('#row').value)
         this.values=[col, row]
-      
     }
 //*****************************************************************************
     tablaMatrix(){
@@ -29,7 +29,7 @@ class CreateMatrix{
             for(let j=0; j<co; j++){
               const c= r.insertCell()
               const inputs= document.createElement("input")//creo el input 
-              inputs.type="text" //le asigno un tipo
+              inputs.type="number" //le asigno un tipo
               inputs.size=1// le asigno su size
               c.appendChild(inputs)
             }
@@ -42,15 +42,14 @@ class CreateMatrix{
           const tab= document.getElementById("tab")
           //tab.innerHTML=""
           tab.appendChild(table)
-          //tab.appendChild(document.createElement("br"));
-       //   i++
+   //tab.appendChild(document.createElement("br"));
     }
 //*****************************************************************************
-    matric(){
+    sumaMatrixId(){
+       
+    }
+//*****************************************************************************
 
-    }
-//*****************************************************************************
-   
 }
 //----------------------------------------------------------------------------
 
@@ -68,40 +67,70 @@ function myMatrix() {
     calcu.type="submit"
     calcu.name="calcu"
     calcu.id="calculo"
-
-    
 //----------------------------------------------------------------------------
     const CalcularSuma= document.getElementById("tab")
     CalcularSuma.appendChild(calcu)
 
     const calculo = document.getElementById("calculo");
+
     calculo.addEventListener("click", function(){
         //llamo la tabla con id= "id1"
-        const tablaId1 = document.getElementById("id1");
-        const matrizValores = [];
-        
-        for (const tr of tablaId1.querySelectorAll('tr')) {
-          const filaValores = [];
-          for (const td of tr.querySelectorAll('td')) {
-            const input = td.querySelector('input');
-            if (input) {
-              filaValores.push(input.value);
+        const tablaId=document.getElementById("id1")
+        const tablaMatrixId=[]
+        //console.log(tablaId)
+       
+        for(const tr of tablaId.querySelectorAll("tr")){
+            const filasId=[]
+            const inputsId = Array.from(tr.querySelectorAll("td input"))
+            for(const valoreInputs of inputsId){
+                if (valoreInputs){
+                    filasId.push(valoreInputs.value)
+                }
             }
-          }
-          if (filaValores.length > 0) {
-            matrizValores.push(filaValores);
-          }
-        }
-        
-        console.log(matrizValores);
-}        /*const sumaMatrix=document.createElement("table")
-        const filas=sumaMatrix.querySelector("tr")
-        const valores=[]*/
+            if(filasId.length > 0){
+            tablaMatrixId.push(filasId)
+            }
+        } 
 
+        const tablaId2=document.getElementById("id2")
+        const tablaMatrixId2=[]
+
+        for(const tr of tablaId2.querySelectorAll("tr")){
+            const filasId=[]
+            const inputsId = Array.from(tr.querySelectorAll("td input"))
+            for(const valoreInputs of inputsId){
+                if (valoreInputs){
+                    filasId.push(valoreInputs.value)
+                }
+            }
+            if(filasId.length > 0){
+            tablaMatrixId2.push(filasId)
+            }
+        } 
+       
+        
+
+        function suma(Array1,Array2){
+
+            const result = []
+            for (let i = 0; i < Array1.length; i++) {
+                result.push([])
+        
+                for (let j = 0; j < Array1[i].length; j++) {
+            
+                    result[i][j]= parseFloat(Array1[i][j])+parseFloat(Array2[i][j])
+        
+                }
+            }
+            return result
+        }
+
+        console.log(tablaMatrixId)
+        console.log(tablaMatrixId2)
+        const resultado =suma(tablaMatrixId,tablaMatrixId2)
+        console.log(resultado)
+    }       
         
     )
 }
-
-
-
 
